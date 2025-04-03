8d27e6d6d43a4426fc18e8c86ed41f35aa558c64
Rohit <katrohit@gmail.com>
2025-04-03 22:59:06 +0530

    fixed wrong api key (:((()

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://wpckzzjlhusvhmckrwen.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwY2t6empsa3Vzdmhtc2tyd2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MTcwMDUsImV4cCI6MjA1OTA5MzAwNX0.uIsnw6DnFBLDFp_UDB_H2rJYyUge6L8E1KlX30Z6gyg";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwY2t6empsaHVzdmhtY2tyd2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MTcwMDUsImV4cCI6MjA1OTA5MzAwNX0.uIsnw6DnFBLDFp_UDB_H2rJYyUge6L8E1KlX30Z6gyg";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
